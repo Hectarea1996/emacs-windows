a4c32aa23c15b70fb9ac4b91bcb8c60407f61f5f
Hectarea <hectometrocuadrado@gmail.com>
2023-03-10 11:30:15 +0100

    First commit

---
+++ elpa/modus-themes-20230308.547/modus-themes-pkg.el
@@ -0,0 +1,12 @@
+(define-package "modus-themes" "20230308.547" "Elegant, highly legible and customizable themes"
+  '((emacs "27.1"))
+  :commit "d4e70e069b993ec56f60cf75d8c147c0df539b8a" :authors
+  '(("Protesilaos Stavrou" . "info@protesilaos.com"))
+  :maintainer
+  '("Modus-Themes Development" . "~protesilaos/modus-themes@lists.sr.ht")
+  :keywords
+  '("faces" "theme" "accessibility")
+  :url "https://git.sr.ht/~protesilaos/modus-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
