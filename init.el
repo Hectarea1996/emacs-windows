958781c5239b7869083fdbfd33f7bbe285a5c402
Hectarea <hectometrocuadrado@gmail.com>
2023-07-16 18:42:52 +0200

    Created implement function and point (not tested)

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes
    '("dde643b0efb339c0de5645a2bc2e8b4176976d5298065b8e6ca45bc4ddf188b7" "3199be8536de4a8300eaf9ce6d864a35aa802088c0925e944e2b74a574c68fd0" "a0415d8fc6aeec455376f0cbcc1bee5f8c408295d1c2b9a1336db6947b89dd98" default))
  '(package-selected-packages
-   '(rg emr srefactor company-lsp vscode-dark-plus-theme flycheck-clang-tidy yasnippet treemacs-projectile treemacs clang-format+ company ue lsp-ivy lsp-ui lsp-mode all-the-icons-ivy-rich dired-hide-dotfiles all-the-icons-dired all-the-icons exwm dirtrack ivy slime avy markdown-mode flycheck-pkg-config undo-tree ivy-xref dumb-jump flycheck modern-cpp-font-lock auto-complete pdf-continuous-scroll-mode pdf-tools paredit parinfer-rust multiple-cursors cmake-mode which-key use-package spacemacs-theme solo-jazz-theme solarized-theme rainbow-delimiters projectile parinfer-rust-mode one-themes modus-themes ivy-rich helpful doom-themes doom-modeline counsel))
+   '(csharp-mode tree-sitter-indent tree-sitter-langs tree-sitter rg emr srefactor company-lsp vscode-dark-plus-theme flycheck-clang-tidy yasnippet treemacs-projectile treemacs clang-format+ company ue lsp-ivy lsp-ui lsp-mode all-the-icons-ivy-rich dired-hide-dotfiles all-the-icons-dired all-the-icons exwm dirtrack ivy slime avy markdown-mode flycheck-pkg-config undo-tree ivy-xref dumb-jump flycheck modern-cpp-font-lock auto-complete pdf-continuous-scroll-mode pdf-tools paredit parinfer-rust multiple-cursors cmake-mode which-key use-package spacemacs-theme solo-jazz-theme solarized-theme rainbow-delimiters projectile parinfer-rust-mode one-themes modus-themes ivy-rich helpful doom-themes doom-modeline counsel))
  '(undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree-history/"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -456,7 +456,7 @@
 		 (project-root (projectile-project-root))
 		 (project-name (projectile-project-name))
 		 (project-file (concat project-root project-name ".uproject")))
-	(concat unreal-executable " " "\"" project-file "\"")))
+	(concat "\"" unreal-executable "\"" " " "\"" project-file "\"")))
 
 ;; Runs a project
 (defun run-unreal-project ()
@@ -517,7 +517,313 @@
 			  ("RET" . newline))
   :hook ((c-mode . my-c-mode-common-hook)
 		 (c++-mode . my-c-mode-common-hook)))
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
+;; ------ tree-sitter ------
+(use-package tree-sitter :ensure t)
+(use-package tree-sitter-langs :ensure t)
+(use-package tree-sitter-indent :ensure t)
+(add-hook 'c++-mode #'tree-sitter-mode)
+(add-hook 'c-mode #'tree-sitter-mode)
+(add-hook 'tree-sitter-mode #'tree-sitter-hl-mode)
+(defvar cpp-language (alist-get 'cpp tree-sitter-languages))
+
+(setq variable-macros-regexp "[[:graph:]]+_API")
+(setq function-macros-regexp "GENERATED_BODY\\\|UFUNCTION\\|UCLASS\\\|UPROPERTY\\\|USTRUCT\\\|UENUM\\\|UMETA\\\|UPARAM")
+
+(defmacro with-no-macros-buffer (buffer new-buffer &rest body)
+  (unless (symbolp new-buffer)
+	(signal 'wrong-type-argument (list 'symbolp new-buffer)))
+  (let ((src-buffer (make-symbol "src-buffer"))
+		(current-point (make-symbol "current-point")))
+	`(let* ((,src-buffer ,buffer)
+			(,current-point (with-current-buffer ,src-buffer
+							 (point))))
+	   (when (not ,src-buffer)
+		 (setq ,src-buffer (current-buffer)))
+	   (let ((,new-buffer (generate-new-buffer "*no-macros-buffer*" t)))
+		 (with-current-buffer ,new-buffer
+		   (unwind-protect
+			   (let ((critical-regions nil))
+				 (insert-buffer ,src-buffer)
+				 (while (re-search-forward variable-macros-regexp nil t)
+				   (let ((end-macro (point))
+						 (start-macro nil))
+					 (save-excursion
+					   (backward-sexp)
+					   (setq start-macro (point)))
+					 (push (cons start-macro (- end-macro start-macro)) critical-regions)))
+				 (goto-char (point-min))
+				 (while (re-search-forward function-macros-regexp nil t)
+				   (let ((end-macro nil)
+						 (start-macro nil))
+					 (save-excursion
+					   (backward-sexp)
+					   (setq start-macro (point)))
+					 (save-excursion
+					   (forward-sexp)
+					   (setq end-macro (point)))
+					 (push (cons start-macro (- end-macro start-macro)) critical-regions)))
+				 (dolist (critical-region critical-regions)
+				   (let ((start-region  (car critical-region))
+						 (length-region (cdr critical-region)))
+					 (goto-char start-region)
+					 (delete-forward-char length-region)
+					 (insert (make-string length-region ?\s))))
+				 (goto-char ,current-point)
+				 (c++-mode)
+				 (tree-sitter-mode)
+				 ,@body)
+			 (and (buffer-name ,new-buffer)
+				  (kill-buffer ,new-buffer))))))))
+
+(defun ue-class-name-at-point ()
+  (interactive)
+  (with-no-macros-buffer (current-buffer) no-unreal-buffer
+						 (when-let* ((uclass-query (tsc-make-query cpp-language "(class_specifier (type_identifier) @x . (base_class_clause)? . (field_declaration_list))"))
+									 (node-at-point (tree-sitter-node-at-pos 'class_specifier))
+									 (captures (tsc-query-captures uclass-query node-at-point 'tsc--buffer-substring-no-properties)))
+						   (tsc-node-text (cdr (aref captures 0))))))
+
+;; "[(field_declaration (function_declarator (field_identifier) @y)) (declaration (function_declarator (identifier) @x))]"
+
+(defun ue-method-name-at-point ()
+  (interactive)
+  (with-no-macros-buffer (current-buffer) no-unreal-buffer
+						 (when-let* ((method-query (tsc-make-query cpp-language "(field_declaration (function_declarator (field_identifier) @y))"))
+									 (node-at-point (tree-sitter-node-at-pos 'field_declaration))
+									 (captures (tsc-query-captures method-query node-at-point 'tsc--buffer-substring-no-properties)))
+						   (if (not (seq-empty-p captures))
+							   (tsc-node-text (cdr (elt captures 0)))
+							 (when-let* ((constructor-query (tsc-make-query cpp-language "(declaration (function_declarator (identifier) @x))"))
+										 (node-at-point (tree-sitter-node-at-pos 'declaration))
+										 (captures (tsc-query-captures constructor-query node-at-point 'tsc--buffer-substring-no-properties)))
+							   (tsc-node-text (cdr (elt captures 0))))))))
+
+(defun ue-method-node-at-point ()
+  (interactive)
+  (with-no-macros-buffer (current-buffer) no-unreal-buffer
+						 (when-let* ((method-query (tsc-make-query cpp-language "(field_declaration (function_declarator (field_identifier))) @x"))
+									 (node-at-point (tree-sitter-node-at-pos 'field_declaration))
+									 (captures (tsc-query-captures method-query node-at-point 'tsc--buffer-substring-no-properties)))
+						   (if (not (seq-empty-p captures))
+							   (cdr (elt captures 0))
+							 (when-let* ((constructor-query (tsc-make-query cpp-language "(declaration (function_declarator (identifier))) @x"))
+										 (node-at-point (tree-sitter-node-at-pos 'declaration))
+										 (captures (tsc-query-captures constructor-query node-at-point 'tsc--buffer-substring-no-properties)))
+							   (cdr (elt captures 0)))))))
+
+(defun ue-buffer-node-classes (&optional buffer)
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (with-no-macros-buffer buffer no-ue-buffer
+						 (when-let* ((uclass-query (tsc-make-query cpp-language "(class_specifier (type_identifier) . (base_class_clause)? . (field_declaration_list)) @x"))
+									 (root-node (tsc-root-node tree-sitter-tree))
+									 (captures (tsc-query-captures uclass-query root-node 'tsc--buffer-substring-no-properties)))
+						   (seq-map (lambda (elem)
+									  (print (tsc-node-text (cdr elem)) (get-buffer "*scratch*"))
+									  (cdr elem))
+									captures))))
+
+(defun ue-node-class-name (class-node)
+  (unless (tsc-node-p class-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p class-node)))
+  (when-let* ((name-query (tsc-make-query cpp-language "(class_specifier (type_identifier) @x . (base_class_clause)? . (field_declaration_list))"))
+			  (captures (tsc-query-captures name-query elem 'tsc--buffer-substring-no-properties)))
+	(tsc-node-text (cdr (elt captures 0)))))
+
+(defun ue-buffer-class-names (&optional buffer)
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (seq-mapcat (lambda (elem)
+				(when-let ((name (ue-node-class-name elem)))
+				  (list name)))
+			  (ue-buffer-node-classes buffer)))
+
+(defun ue-node-class-node-methods (class-node)
+  (unless (tsc-node-p class-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p class-node)))
+  (when-let* ((method-query (tsc-make-query cpp-language "[(field_declaration (function_declarator (field_identifier) @y)) (declaration (function_declarator (identifier) @x))]"))
+			  (captures (tsc-query-captures method-query class-node 'tsc--buffer-substring-no-properties)))
+	(seq-map (lambda (elem)
+			   (cdr elem))
+			 captures)))
+
+(defun ue-node-method-name (method-node)
+  (unless (tsc-node-p method-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p method-node)))
+  (when-let* ((method-query (tsc-make-query cpp-language "[(field_declaration (function_declarator (field_identifier) @x)) (declaration (function_declarator (identifier) @y))]"))
+			  (captures (tsc-query-captures method-query method-node 'tsc--buffer-substring-no-properties)))
+	(seq-map (lambda (elem)
+			   (tsc-node-text (cdr elem)))
+			 captures)))
+
+(defun ue-node-class-method-names (class-node)
+  (unless (tsc-node-p class-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p class-node)))
+  (seq-map (lambda (elem)
+			 (ue-node-method-name elem))
+		   (ue-node-class-node-methods class-node)))
+
+(defun ue-buffer-method-names (&optional buffer)
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (seq-mapcat (lambda (elem)
+				(print (tsc-node-text elem) (get-buffer "*scratch*"))
+				(ue-node-class-method-names elem))
+			  (ue-buffer-node-classes buffer)))
+
+(defun ue-node-class-declaration-names (class-node)
+  (unless (tsc-node-p class-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p class-node)))
+  (let ((class-name (ue-node-class-name class-node))
+		(method-names (ue-node-class-method-names class-node)))
+	(seq-map (lambda (method-name)
+			   (cons class-name method-name))
+			 method-names)))
+
+(defun ue-buffer-declaration-names (&optional buffer)
+  "Returns a list of pair (class-name . method-name)"
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (let ((node-classes (ue-buffer-node-classes buffer)))
+	(seq-mapcat (lambda (node-class)
+				  (ue-node-class-declaration-names node-class))
+				node-classes)))
+
+(defun ue-buffer-node-definitions (&optional buffer)
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (with-no-macros-buffer buffer no-macros-buffer
+						 (when-let* ((definition-query (tsc-make-query cpp-language "(function_definition) @x"))
+									 (root-node (tsc-root-node tree-sitter-tree))
+									 (captures (tsc-query-captures definition-query root-node 'tsc--buffer-substring-no-properties)))
+						   (seq-map 'cdr captures))))
+
+(defun ue-node-definition-class-name (definition-node)
+  (unless (tsc-node-p definition-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p definition-node)))
+  (when-let* ((definition-class-query (tsc-make-query cpp-language "(function_declarator (qualified_identifier (namespace_identifier) @x))"))		  
+			  (captures (tsc-query-captures definition-class-query definition-node 'tsc--buffer-substring-no-properties)))
+	(let ((class-node (cdr (elt captures 0))))
+	  (tsc-node-text class-node))))
+
+(defun ue-node-definition-method-name (definition-node)
+  (unless (tsc-node-p definition-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p definition-node)))
+  (when-let* ((definition-method-query (tsc-make-query cpp-language "(function_declarator (qualified_identifier (identifier) @x))"))		  
+			  (captures (tsc-query-captures definition-method-query definition-node 'tsc--buffer-substring-no-properties)))
+	(let ((method-node (cdr (elt captures 0))))
+	  (tsc-node-text method-node))))
+
+(defun ue-node-definition-names-and-positions (definition-node)
+  "Returns a list of four elements: (class-name method-name start-position end-position)"
+  (unless (tsc-node-p definition-node)
+	(signal 'wrong-type-argument (list 'tsc-node-p definition-node)))
+  (let ((class-name (ue-node-definition-class-name definition-node))
+		(method-name (ue-node-definition-method-name definition-node))
+		(start-position (tsc-node-start-position definition-node))
+		(end-position (tsc-node-end-position definition-node)))
+	(list class-name method-name start-position end-position)))
+
+(defun ue-buffer-definition-names-and-positions (&optional buffer)
+  (when (not buffer)
+	(setq buffer (current-buffer)))
+  (seq-map 'ue-node-definition-names-and-positions (ue-buffer-node-definitions buffer)))
+
+(defun ue-definition-points (current-def-names goal-def-names declaration-names)
+  (let ((current-def-class (car current-def-names))
+		(current-def-method (cdr current-def-names))
+		(goal-def-class (car goal-def-names))
+		(goal-def-method (cdr goal-def-names))
+		(current-points 0)
+		(points-multiplier 1)
+		(current-names-found nil))
+	(while (and (not (null declaration-names))
+				(not current-names-found))
+	  
+	  (let ((declaration-class (caar declaration-names))
+			(declaration-method (cdar declaration-names)))
+		(cond
+		 ((and (string-equal current-def-class declaration-class)
+			   (string-equal current-def-method declaration-method))
+		  (setq current-names-found t))
+		 ((and (string-equal goal-def-class declaration-class)
+			   (string-equal goal-def-method declaration-method))
+		  (setq points-multiplier -1))
+		 (t
+		  (setq current-points (+ current-points 1)))))
+	  (setq declaration-names (cdr declaration-names)))
+	(if current-names-found
+		(* current-points points-multiplier)
+	  nil)))
+
+(defun ue-best-definition-info (definitions-info goal-def-names declaration-names)
+  (let* ((first-def-names (cons (first (car definitions-info)) (second (car definitions-info))))
+		 (max-points (ue-definition-points first-def-names goal-def-names declaration-names))
+		 (best-definition (car definitions-info))
+		 (declaration-names (cdr declaration-names)))
+	(while (not (null definitions-info))
+	  (let* ((current-def-names (cons (first (car definitions-info)) (second (car definitions-info))))
+			 (current-points (ue-definition-points current-def-names goal-def-names declaration-names)))
+		(when (and current-points (> current-points max-points))
+		  (setq max-points current-points)
+		  (setq best-definition (car definitions-info))))
+	  (setq definitions-info (cdr definitions-info)))
+	(and max-points
+		 (cons best-definition max-points))))
+
+(defun ue-best-definition-position (definitions-info goal-def-names declaration-names)
+  (let ((best-definition-and-points (ue-best-definition-info definitions-info goal-def-names declaration-names)))
+	(and best-definition-and-points
+		 (let* ((best-definition (car best-definition-and-points))
+				(points (cdr best-definition-and-points))
+				(best-start-position (third best-definition))
+				(best-end-position (fourth best-definition)))
+		   (if (< points 0)
+			   best-start-position
+			 best-end-position)))))
+
+(defun ue-implement-function-at-point ()
+  (interactive)
+  (let ((declaration-at-point (ue-)))))
+
+(defun function-declaration-to-implementation (function-str class-name)
+  (with-temp-buffer
+	(insert function-str)
+	(goto-char (point-min))
+	(replace-regexp "virtual\\\|override\\\|static\\\|;" "")
+	(c++-mode)
+	(tree-sitter-mode)
+	(let* ((function-name-query (tsc-make-query tree-sitter-language "(function_declarator (identifier) @x)"))
+		   (function-captures (tsc-query-captures function-name-query (tsc-root-node tree-sitter-tree) 'tsc--buffer-substring-no-properties))
+		   (function-name-position (tsc-node-start-position (cdr (aref function-captures 0)))))
+	  (goto-char function-name-position)
+	  (insert class-name "::")
+	  (goto-char (point-max))
+	  (newline)
+	  (insert "{" ?\n "}"))
+	(buffer-string)))
+
+(defun ue-implement-function-at-point ()
+  (interactive)
+  (when-let* ((class-name (ue-get-class-at-point))
+			  (method-node (ue-method-node-at-point))
+			  (method-name (ue-method-name-at-point))
+			  (function-implementation (function-declaration-to-implementation (tsc-node-text method-node)))
+			  (declaration-names (ue-buffer-declaration-names))
+			  (goal-def-names (cons class-name method-name)))
+	(projectile-find-other-file)
+	(let* ((definitions-info (ue-buffer-definition-names-and-positions))
+		   (best-position (ue-best-definition-position definitions-info goal-def-names declaration-names)))
+	  (goto-char (or best-position (point-max)))
+	  (insert ?\n ?\n function-implementation ?\n ?\n))))
+
+;; ------ csharp-mode ------
+(use-package csharp-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode)))
 
 ;; ------ flycheck ------
 (use-package flycheck
@@ -533,7 +839,8 @@
 
 ;; ------ company ------
 (use-package company
-  :hook (lsp . company)
+  :hook ((lsp . company)
+		 (emacs-lisp-mode . company-mode))
   :bind (:map company-active-map
               ("<tab>" . company-complete-selection))
   :config
